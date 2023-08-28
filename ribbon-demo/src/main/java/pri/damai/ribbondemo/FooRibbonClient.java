2b6052cd4a026360be5772c0797f5e66545e31c6
YangXinFu <www_yxf@foxmail.com>
2023-08-28 08:52:00 +0800

    feat: .

--- ribbon-demo/src/main/java/pri/damai/ribbondemo/FooRibbonClient.java
+++ ribbon-demo/src/main/java/pri/damai/ribbondemo/FooRibbonClient.java
@@ -1,16 +1,18 @@
 package pri.damai.ribbondemo;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * @author YangXinFu
  * @date 2023/8/23 17:05
  */
-@FeignClient(name = "baidu")
+@FeignClient(name = "damai")
 public interface FooRibbonClient {
 
-    @GetMapping
-    String homePage();
+    @GetMapping("/printLog/{log}")
+    String homePage(@PathVariable String log);
 
 }
--- ribbon-demo/src/main/java/pri/damai/ribbondemo/RibbonDemoApplication.java
+++ ribbon-demo/src/main/java/pri/damai/ribbondemo/RibbonDemoApplication.java
@@ -2,8 +2,11 @@ package pri.damai.ribbondemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +22,19 @@ public class RibbonDemoApplication {
         SpringApplication.run(RibbonDemoApplication.class, args);
     }
 
+//    @Resource
+//    SpringClientFactory springClientFactory;
     @Resource
     FooRibbonClient fooRibbonClient;
+    @GetMapping("/test/{log}")
+    public String homePage(@PathVariable String log) {
+        return fooRibbonClient.homePage(log);
+    }
 
-    @GetMapping("/homePage")
-    public String homePage() {
-        System.out.println("sss");
-        return fooRibbonClient.homePage();
+    @GetMapping("/printLog/{log}")
+    public void printLog(@PathVariable String log) {
+//        ILoadBalancer loadBalancer = springClientFactory.getLoadBalancer("damai");
+//        Server server = loadBalancer.chooseServer(null);
+//        System.out.println(server);
     }
 }
